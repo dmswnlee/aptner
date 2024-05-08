8d7edd86b56b643c3c1ce38f8d44efb0fce2f1c2
LeeEunJoo <dlrmawnl03@gmail.com>
2024-05-08 10:39:44 +0900

    Add : MSW 파일 생성 및 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
+import { MSWComponent } from './_component/MSWComponent';
 import "./globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="ko">
-			<body>{children}</body>
+			<body>
+				<MSWComponent />
+				{children}
+			</body>
 		</html>
 	);
 }
