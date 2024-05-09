4337f360e10a3cb9b36bdae4542063b775fe0a64
LeeEunJoo <dlrmawnl03@gmail.com>
2024-05-09 17:07:04 +0900

    Layout : 로그인 메인

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,10 @@
 "use client";
+import ColorButton from "@/components/buttons/ColorButton";
 import LargeBorderButton from "@/components/buttons/LargeBorderButton";
-import LargeColorButton from "@/components/buttons/LargeColorButton";
 import Input from "@/components/input/Input";
+import Link from "next/link";
 import { useState } from "react";
+import { AiTwotoneNotification } from "react-icons/ai";
 
 const login = () => {
 	const handleChange = () => {};
@@ -16,7 +18,7 @@ const login = () => {
 	};
 
 	return (
-		<div className="mt-[150px] flex justify-center">
+		<div className="mt-[178px] flex justify-center">
 			<div className="w-[1080px] flex flex-col items-center">
 				<div className="w-full px-[24px] mb-[100px] border-solid border-b-2 border-[#222222]">
 					<h2 className="text-4xl font-semibold pb-[16px]">로그인</h2>
@@ -25,7 +27,7 @@ const login = () => {
 					<Input type="email" placeholder="아이디 입력" onChange={handleChange} />
 					<Input type="email" placeholder="비밀번호 입력" onChange={handleChange} />
 					<div className="mt-[56px]">
-						<LargeColorButton text="로그인" />
+						<ColorButton text="로그인" size="lg" />
 					</div>
 					<div className="w-full mt-[20px] flex justify-between text-gray_400">
 						<div className="flex items-center gap-[15px]">
@@ -33,16 +35,34 @@ const login = () => {
 								type="checkbox"
 								checked={isChecked}
 								onChange={handleChangeCheck}
-								className={`w-[18px] h-[18px] ${isChecked ? 'bg-blue_300' : 'bg-gray_400'}`}
+								className={`w-[18px] h-[18px] ${isChecked ? "bg-blue_300" : "bg-gray_400"}`}
 							/>
-							<label htmlFor="check" className={`${isChecked ? 'text-blue_300' : 'text-gray_400'}`}>자동로그인</label>
+							<label htmlFor="check" className={`${isChecked ? "text-blue_300" : "text-gray_400"}`}>
+								자동로그인
+							</label>
 						</div>
 						<div className="flex gap-[10px]">
-							<button>아이디 찾기</button>
+							<Link href="/login/find-id-password">아이디 찾기</Link>
 							<div className="w-[1px] bg-[#ccc]"></div>
 							<button>비밀번호 찾기</button>
 						</div>
 					</div>
+					{isChecked && (
+						<div className="flex gap-[5px] w-[292px] p-[10px] bg-white shadow-lg rounded-[5px]">
+							<AiTwotoneNotification />
+							<div className="flex flex-col gap-[5px] text-gray_07">
+								<p className="text-[12px]">
+									자동로그인을 사용하시면 다음부터 회원아이디와
+									<br />
+									비밀번호를 입력하실 필요가 없습니다.
+									<br />
+									공공장소에서는 개인정보가 유출될 수 있으니 사용
+									<br />을 자제하여 주십시오.
+								</p>
+								<p className="font-semibold">자동로그인을 사용하시겠습니까?</p>
+							</div>
+						</div>
+					)}
 					<div className="mt-[50px]">
 						<p className="text-gray_400">회원이 아니시라면?</p>
 						<div className="mt-[10px]">
