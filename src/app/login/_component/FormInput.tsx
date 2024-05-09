5254f08729cc46601290028390c15fed37defaae
LeeEunJoo <dlrmawnl03@gmail.com>
2024-05-09 17:06:51 +0900

    Layout : 아이디/비밀번호 찾기

---
+++ src/app/login/_component/FormInput.tsx
@@ -0,0 +1,23 @@
+interface InputProps {
+	title: string;
+	placeholder?: string;
+	size: "lg" | "md";
+}
+
+const container = "flex gap-[10px]";
+const labelStyle = "w-[100px] p-[10px]";
+const inputStyle =
+	"outline-none border border-solid border-[#BBBBBB] px-[30px] py-[15px] rounded-main";
+
+const FormInput = ({ title, placeholder, size }: InputProps) => {
+	const inputSize = size === "lg" ? "w-[430px] h-[48px]" : "w-[292px] h-[48px]";
+
+	return (
+		<div className={`${container}`}>
+			<p className={`${labelStyle}`}>{title}</p>
+			<input type="text" placeholder={placeholder} className={`${inputStyle} ${inputSize}`} />
+		</div>
+	);
+};
+
+export default FormInput;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,35 +1,35 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        theme: "#174d50",
-        charcoal: "#4c5d71",
-        border : "#eaeaea",
-        placeholder : "#fcfcfc",
-        blue_05 : "#00A8FF",
-        blue_300 : "#05A8FF",
-        blue_500 : "#0086CC",
-        gray_200 : "#cccccc",
-        gray_300 : "#C8C8C8",
-        gray_400 : "#858E96",
-        gray_600 : "#777777",
-        gray_800 : "#333333"
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+			colors: {
+				theme: "#174d50",
+				charcoal: "#4c5d71",
+				border: "#eaeaea",
+				placeholder: "#fcfcfc",
+				blue_05: "#00A8FF",
+				blue_300: "#05A8FF",
+				blue_500: "#0086CC",
+				gray_07: "#999999",
+				gray_200: "#cccccc",
+				gray_300: "#C8C8C8",
+				gray_400: "#858E96",
+				gray_600: "#777777",
+				gray_800: "#333333",
+			}
+		},
+	},
+	plugins: [],
 };
 
 export default config;
