60b4d7ce4191f30115df0f979b042805c1e4aa3d
LeeEunJoo <dlrmawnl03@gmail.com>
2024-05-12 17:41:51 +0900

    Layout : 공지사항 페이지

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -1,25 +1,13 @@
-import Posts from "@/components/noticeboard/Posts";
-import Tabs from "@/components/noticeboard/Tabs";
+import NoticeDetail from './_component/NoticeDetail';
+import NoticeMain from './_component/NoticeMain';
 
 const Notice = () => {
-	const tabs = [
-		{ name: "all", label: "전체" },
-		{ name: "sharing", label: "공동생활" },
-		{ name: "construction", label: "공사안내" },
-		{ name: "management", label: "관리사무소" },
-		{ name: "representative", label: "입대위" },
-		{ name: "election-commission", label: "선관위" },
-		{ name: "result", label: "회의 결과" },
-	];
-
+	
 	return (
 		<div className="mt-[178px] flex justify-center">
 			<div className="w-[1080px] flex flex-col gap-10">
 				<h2 className="text-2xl">공지사항</h2>
-				<div>
-					<Tabs tabs={tabs} />
-					<Posts />
-				</div>
+				<NoticeDetail />
 			</div>
 		</div>
 	);
