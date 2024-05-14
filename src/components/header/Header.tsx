21eb57aee669806e8e367db9052d94d780e58da3
정은 <chloekje22@gmail.com>
2024-05-14 14:00:53 +0900

    fix: header 디자인 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,29 @@
+'use client'
 import Searchbar from "@/components/header/Searchbar"
 import Navbar from "@/components/header/Navbar"
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '@/assets/images/logo.png';
+import { useState } from "react";
+import Search from "@/components/Search";
 
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  
   return (
-    <div className="flex-column w-full fixed top-0">
-      <Searchbar />
+    <div className="flex-column">
+      <div className="flex justify-center items-center h-[80px] bg-theme">
+        <div className="flex justify-center items-center ml-[500px]">
+          <Link href="/">
+            <Image src={logo} alt="Logo" className="w-[167px] h-[27px]" /> 
+          </Link>
+        </div>
+        {isLoggedIn && (<div className="ml-[400px]">
+          <Link href="/login" className="text-xs text-white mr-2">로그인</Link>
+          <span className="text-white mx-2">|</span>
+          <Link href="/join" className="text-xs text-white ml-2">회원가입</Link>
+        </div>)}
+      </div>
       <Navbar />
     </div>
   )
--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,27 +1,23 @@
-import Link from 'next/link';
+import { FaSearch } from 'react-icons/fa';
+import Link from "next/link";
 import Image from 'next/image';
 import logo from '@/assets/images/logo.png';
+import Search from '../Search';
 
 const Navbar = () => {
   return (
-    <nav className="bg-white border-b-2 border-theme w-full h-20 flex items-center justify-between">
-      <div className="flex w-[800px] mx-auto justify-between">
-        <div className="flex items-center">
-          <Link href="/">
-            <div className="flex items-center">
-              <Image src={logo} alt="PRUGIO Logo" width={100} height={40} /> {/* Adjust width and height as needed */}
-            </div>
-          </Link>
-        </div>
-        <div className="sm:block space-x-12 text-sm self-center font-semibold">
-          <Link href="/" className="ml-2 text-charcoal">전체보기</Link>
-          <Link href="/apartment" className="ml-2 text-charcoal">아파트 소개</Link>
-          <Link href="/notice" className="ml-2 text-charcoal">공지사항</Link>
-          <Link href="/disclosure" className="ml-2 text-charcoal">의무공개</Link>
-          <Link href="/communication" className="ml-2 text-charcoal">소통공간</Link>
-          <Link href="/complaints" className="ml-2 text-charcoal">민원게시판</Link>
+    <nav className="bg-white border-b-2 border-theme w-full h-20 flex justify-center items-center gap-[32px]">
+      <div className="flex w-[756px] h-[18px] justify-center">
+        <div className="sm:block space-x-[56px]">
+          <Link href="/" className=" text-charcoal">전체보기</Link>
+          <Link href="/apartment" className=" text-charcoal">아파트 소개</Link>
+          <Link href="/notice" className=" text-charcoal">공지사항</Link>
+          <Link href="/disclosure" className=" text-charcoal">의무공개</Link>
+          <Link href="/communication" className=" text-charcoal">소통공간</Link>
+          <Link href="/complaints" className=" text-charcoal">민원게시판</Link>
         </div>
       </div>
+      <Search />
     </nav> 
   );
 };
