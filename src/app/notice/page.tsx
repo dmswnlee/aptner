ceba2eaef18a0a69001ed7b32a82f053897d8e2f
LeeEunJoo <dlrmawnl03@gmail.com>
2024-05-14 15:46:19 +0900

    Layout : 레이아웃 수정

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -4,7 +4,7 @@ import NoticeMain from './_component/NoticeMain';
 const Notice = () => {
 	
 	return (
-		<div className="mt-[178px] flex justify-center">
+		<div className="mt-12 flex justify-center">
 			<div className="w-[1080px] flex flex-col gap-10">
 				<h2 className="text-2xl">공지사항</h2>
 				<NoticeDetail />
