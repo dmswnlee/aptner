66d10ad9688dafd33851590d69a4ac00b25e2854
LeeEunJoo <dlrmawnl03@gmail.com>
2024-05-15 19:31:55 +0900

    Feat : disabled 조건 추가

--- src/components/buttons/ColorButton.tsx
+++ src/components/buttons/ColorButton.tsx
@@ -2,16 +2,22 @@ interface Props {
 	text: string;
 	onClick?: () => void;
 	size: "lg" | "md";
+	disabled?: any;
 }
 
-const ColorButton = ({ text, onClick, size }: Props) => {
+const ColorButton = ({ text, onClick, size, disabled }: Props) => {
 	const buttonSize = size === "lg" ? "w-[430px] h-[58px]" : "w-[214px] h-[48px]";
+	let buttonStyle = "text-white bg-blue_300 rounded-[5px] hover:bg-blue_500";
+
+	if (disabled) {
+		buttonStyle = "text-[#555555] bg-gray_05 cursor-not-allowed";
+	}
 
 	return (
-		<button className={`text-white bg-blue_300 rounded-[5px] hover:bg-blue_500 ${buttonSize}`} onClick={onClick}>
+		<button className={`rounded ${buttonStyle} ${buttonSize}`} onClick={onClick} disabled={disabled}>
 			{text}
 		</button>
 	);
 };
 
-export default ColorButton
+export default ColorButton;
