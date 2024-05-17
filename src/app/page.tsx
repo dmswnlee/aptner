47ed8de5a727fe9a45892f7cc329068706aa42be
정은 <chloekje22@gmail.com>
2024-05-17 13:19:58 +0900

    add: navbar 전체보기 modal 생성 및 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link"
 import Image from 'next/image';
 import home from '@/assets/images/home.png';
 
@@ -6,7 +5,7 @@ const Home = () => {
   return (
     <>
       <div className="flex justify-center">
-        <Image src={home} alt="home" className="w-[1920px] h-[1356px]"/> 
+        <Image src={home} alt="home" className="w-full h-full"/> 
       </div>
     </>
   )
