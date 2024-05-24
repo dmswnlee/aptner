3feeea87d06b08333b8fd0e0ba71b6cb54715a05
LeeEunJoo <dlrmawnl03@gmail.com>
2024-05-24 17:59:25 +0900

    Layout : 전체 레이아웃 배치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,12 @@ import Footer from "@/components/Footer";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="ko">
-			<body>
+		<html lang="ko" className="h-full">
+			<body className="flex flex-col min-h-screen">
 				<MSWComponent />
 				<Header />
 				<ReduxProvider>
-					<main>{children}</main>
+					<main className="flex-1">{children}</main>
 				</ReduxProvider>
         <Footer />
 			</body>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -6,13 +6,12 @@ import IdentityVerification from "./_component/IdentityVerification";
 import SignUpForm from "./_component/SignUpForm";
 import SignUpSuccess from "./_component/SignUpSuccess";
 import { RootState } from "@/stores/store";
-import NoAuthSignUp from './_component/NoAuthSignUp';
 
 const SignUp = () => {
 	const step = useSelector((state: RootState) => state.registration.step);
 
 	return (
-		<div className="mt-12 flex justify-center">
+		<div className="py-12 flex justify-center">
 			<div className="w-[1080px] flex flex-col gap-10">
 				<div className="w-full flex justify-center">
 					<h2 className="text-2xl font-semibold">회원가입</h2>
