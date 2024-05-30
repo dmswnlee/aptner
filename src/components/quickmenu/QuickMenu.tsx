cfdfe9ed74fbb4503d0fb9c31803989ddfea641d
정은 <chloekje22@gmail.com>
2024-05-30 11:31:29 +0900

    Modify: quick menu design

--- src/components/quickmenu/QuickMenu.tsx
+++ src/components/quickmenu/QuickMenu.tsx
@@ -7,26 +7,26 @@ import community from '@/assets/images/quickMenu/community.png';
 
 const QuickMenu = () => {
   return (
-    <div className="fixed right-20 top-1/3 flex flex-col items-center gap-4 p-4 rounded-lg transition-all duration-300 group hover:bg-[#FAFAFA] hover:shadow-lg hover:w-48 bg-transparent">
-      <div className="flex items-center gap-4 w-full hover:bg-[#EBF7FF]">
+    <div className="fixed right-20 top-1/3 flex flex-col items-center gap-2 p-4 rounded-lg transition-all duration-300 group hover:bg-[#FAFAFA] hover:shadow-lg hover:w-48 bg-transparent">
+      <div className="flex items-center gap-3 w-full hover:bg-[#EBF7FF] p-1 hover:rounded-3xl hover:w-[170px]">
         <Image src={car} alt="Car" width={48} height={48} />
         <span className="text-black opacity-0 group-hover:opacity-100 transition-opacity duration-300">
           방문차량 예약
         </span>
       </div>
-      <div className="flex items-center gap-4 w-full hover:bg-[#EBF7FF]">
+      <div className="flex items-center gap-4 w-full hover:bg-[#EBF7FF] p-1 hover:rounded-3xl hover:w-[170px]">
         <Image src={bill} alt="Bill" width={48} height={48} />
         <span className="text-black opacity-0 group-hover:opacity-100 transition-opacity duration-300">
           관리비
         </span>
       </div>
-      <div className="flex items-center gap-4 w-full hover:bg-[#EBF7FF]">
+      <div className="flex items-center gap-4 w-full hover:bg-[#EBF7FF] p-1 hover:rounded-3xl hover:w-[170px]">
         <Image src={calendar} alt="Calendar" width={48} height={48} />
         <span className="text-black opacity-0 group-hover:opacity-100 transition-opacity duration-300">
           일정표
         </span>
       </div>
-      <div className="flex items-center gap-4 w-full hover:bg-[#EBF7FF]">
+      <div className="flex items-center gap-4 w-full hover:bg-[#EBF7FF] p-1 hover:rounded-3xl hover:w-[170px]">
         <Image src={community} alt="Community" width={48} height={48} />
         <span className="text-black opacity-0 group-hover:opacity-100 transition-opacity duration-300">
           커뮤니티 센터
