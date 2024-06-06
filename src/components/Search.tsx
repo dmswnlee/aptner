4c120ebe3fc5bd233f9fbc97b40a8e51830279ce
정은 <chloekje22@gmail.com>
2024-06-07 01:02:29 +0900

    Add: 통합검색 enter

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,26 @@
 "use client"
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useState, KeyboardEvent } from "react";
 import { FaSearch } from "react-icons/fa";
+import { useRouter } from "next/navigation";
 
 const Search = () => {
   const [search, setSearch] = useState("");
+  const router = useRouter();
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value);
+  };
+
+  const handleSearch = () => {
+    if (search.trim() !== "") {
+      router.push(`/mainsearch?query=${search}`);
+    }
+  };
+
+  const handleKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
   };
 
   return (
@@ -16,10 +30,14 @@ const Search = () => {
           type="text"
           value={search}
           onChange={handleInputChange}
+          onKeyPress={handleKeyPress}
           className="text-sm h-[40px] w-[320px] pl-5 rounded-[10px] border border-solid border-[#BBBBBB] focus:outline-none focus:bg-gray-100"
           placeholder="검색하기"
         />
-        <FaSearch className="absolute top-1/2 transform -translate-y-1/2 left-[290px] text-gray-400" />
+        <FaSearch 
+          className="absolute top-1/2 transform -translate-y-1/2 left-[290px] text-gray-400 cursor-pointer"
+          onClick={handleSearch}
+        />
       </div>
     </div>
   );
